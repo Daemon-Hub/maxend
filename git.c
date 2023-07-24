6e600f98af4a699ce3762b29e84c056e31de3781
Daemon-Hub <islamkertov@gmail.com>
2023-07-24 22:37:31 +0300

    init

--- git.c
+++ git.c
@@ -1,7 +1,10 @@
 int main(void)
 {
+	system("git init");
 	system("git add .");
 	system("git commit -m \"init\"");
+	system("git branch -M main");
+	system("git remote add origin https://github.com/Daemon-Hub/maxend.git");
 	system("git push -u origin main");
 	return 0;
 }
