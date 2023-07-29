0d4370b3a5edaa6df9089978abfcc53f54304999
Daemon-Hub <islamkertov@gmail.com>
2023-07-29 15:46:10 +0300

    init

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    class Meta:
+        db_table = "advertisements"
+
+    title = models.CharField("Заголовок", max_length=100)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("Тогр", help_text="Уместен тогр или нет")
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, title={self.title}, price={self.price})"
+
