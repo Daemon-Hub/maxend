a6a0ce187085a7aa4bdc79a6e28dc1cad0c1ec7e
Daemon-Hub <islamkertov@gmail.com>
2023-08-08 15:51:57 +0300

    init

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib import admin
 class Advertisement(models.Model):
     class Meta:
         db_table = "advertisements"
@@ -14,3 +14,34 @@ class Advertisement(models.Model):
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, title={self.title}, price={self.price})"
 
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            from django.utils.html import format_html
+            return format_html(
+                '''
+                    <span style="color: green; font-weight: bold;">
+                        Сегодня в {}
+                    </span>
+                ''',
+                created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.update_at.date() == timezone.now().date():
+            created_time = self.update_at.time().strftime('%H:%M:%S')
+            from django.utils.html import format_html
+            return format_html(
+                '''
+                <span style="color: green; font-weight: bold;">
+                    Сегодня в {}
+                </span>
+                ''',
+                created_time
+            )
+        return self.update_at.strftime('%d.%m.%Y в %H:%M:%S')
